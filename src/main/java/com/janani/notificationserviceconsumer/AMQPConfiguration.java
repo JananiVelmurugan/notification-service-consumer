db318801fe324679bd0ae035917229c1a50d24b3
Janani <jananise@gmail.com>
2017-09-11 16:13:30 +0530

    modified configuration

--- src/main/java/com/janani/notificationserviceconsumer/AMQPConfiguration.java
+++ src/main/java/com/janani/notificationserviceconsumer/AMQPConfiguration.java
@@ -2,19 +2,12 @@ package com.janani.notificationserviceconsumer;
 
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AMQPConfiguration {
-	@Bean
-	public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-		rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
-		return rabbitTemplate;
-	}
 
 	@Bean
 	public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
